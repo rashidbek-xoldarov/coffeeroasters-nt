719cf86297444533d13d7d31d84447b623cc5069
rashid1111111 <rashidbek0309@gmail.com>
2022-07-19 17:01:32 +0500

    Add modal

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const link = document.querySelectorAll(".question-item-title");
 const list = document.querySelectorAll(".ingrediend-list");
 const elList = document.querySelector(".question-list");
+const elModalOpenBtn = document.querySelector(".order-summary-link");
+const elModal = document.querySelector(".modal");
+const elModalCloseBtn = document.querySelector(".checkout-btn");
 
 link.forEach((element) => {
   element.addEventListener("click", function () {
@@ -8,4 +11,17 @@ link.forEach((element) => {
       "ingrediend-list-block"
     );
   });
+});
+
+elModalOpenBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  elModal.classList.add("modal-open");
+});
+
+elModalCloseBtn.addEventListener("click", function () {
+  elModal.classList.remove("modal-open");
+});
+
+elModal.addEventListener("click", function () {
+  elModal.classList.remove("modal-open");
 });
